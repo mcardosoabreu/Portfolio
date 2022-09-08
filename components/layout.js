38ba6fd36ca4ff5bc050ede73c1267bb0586ffbf
Miguel Cardoso Abreu <74303753+mcardosoabreu@users.noreply.github.com>
2022-09-07 21:22:41 -0300

    add google metatags

--- components/layout.js
+++ components/layout.js
@@ -6,13 +6,16 @@ export default function Layout({ children }) {
     return (
         <div>
             <Head>
-                <title>Miguel Cardoso Abreu</title>
                 <link rel="icon" href="/favicon.ico"></link>
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
                 <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Serif:opsz,wght@8..144,700&display=swap" rel="stylesheet"></link>
                 <meta charset="UTF-8"></meta>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <meta name="description" content="Miguel Cardoso Abreu Portfolio"></meta>
+                <meta name="google-site-verification" content="google4aa1e65d98c87fc8"></meta>
+                <meta name="robots" content="noindex,nofollow"></meta>
+                <title>Miguel Cardoso Abreu</title>
             </Head>
 
             <Header></Header>
