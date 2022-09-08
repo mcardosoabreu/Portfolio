d0d327b9d0ff6b5e4c5b10c54bc890cf7d000166
Miguel Cardoso Abreu <74303753+mcardosoabreu@users.noreply.github.com>
2022-09-07 21:56:30 -0300

    Add phone number

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ function Home() {
             <section className={classes.section}>
                 <h2>{t("common:contact")}</h2>
                 <Contact label="email" value="mcardosoabreu@gmail.com"></Contact>
+                <Contact label="phone" value="+55 41 991985305"></Contact>
                 <Contact label="github" value="https://github.com/mcardosoabreu"></Contact>
             </section>
             <br/>
